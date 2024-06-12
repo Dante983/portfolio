cbba12030b33f2fccb9e0b1a4fa562f8327f347a
Nikola Savic <savicn209@gmail.com>
2024-06-13 00:03:16 +0200

    initial commit

---
+++ js/commands.js
@@ -0,0 +1,102 @@
+// var youtube = "https://www.youtube.com/fknight/";
+// var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/nikola-savi%C4%87-10b8b9179/";
+// var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/Dante983";
+var email = "mailto:savicn209@gmail.com";
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!ðŸ‘‹",
+  "I'm a software engineer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documented my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software",
+  "engineering & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creation - strike while",
+  "the iron's hot.",
+  "Now, I make videos about software engineering and computer science, building anything my heart desires,",
+  "like this terminal website.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/SavicNikola' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/SavicNikola (aka Dante983)' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  '<ul id="projects-list">',
+  "  <li>",
+  '    <h2><a href="https://github.com/Dante983/vp-band" target="_blank">Vizantijsko Plavo Band</a></h2>',
+  "    <p>A website that was used for musical band where focus was to have a calendar",
+  " with that shows avaiabled dates for a show",
+  " as well as an admin access where you can add/remove/edit events.</p>",
+  "    <p>Technologies: Node.js, HTML, CSS, MongoDB</p>",
+  "  </li>",
+  "  <li>",
+  '    <h2><a href="https://github.com/Dante983/ai-image-gen" target="_blank">AI Image Generator</a></h2>',
+  "    <p>This is a simple project created to test the openAI image generator.</p>",
+  "    <p>Technologies: Node.js, HTML, CSS</p>",
+  "  </li>",
+  "  <li>",
+  '    <h2><a href="https://login.performcb.com/" target="_blank">Perform[CB]</a></h2>',
+  "    <p>This is an AdTech platform that i work on every day as my main job.</p>",
+  "    <p>Technologies: PHP/Laravel, MySql, MongoDB, Clickhouse, AngularJS, ReactJS, Redis, RabbitMQ</p>",
+  "  </li>",
+  "</ul>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  // '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Welcome to my terminal inspired webpage!',
+  "",
+  "   ____  _____ _____ ___  ____    ____   _____         __           _______      __      ____   ____ _____   ______  ",
+  "  |_   \\|_   _|_   _|_  ||_  _| .'    \\.|_   _|       /  \\         /  ___  |    /  \\    |_  _| |_  _|_   _|./ ___  | ",
+  "    |   \\ | |   | |   | |_/ /  /  .--.  \\ | |        / /\\ \\       |  (__ \\_|   / /\\ \\     \\ \\   / /   | | / ./   \\_| ",
+  "    | |\\ \\| |   | |   |  __'.  | |    | | | |   _   / ____ \\       '.___\\-.   / ____ \\     \\ \\ / /    | | | |        ",
+  "   _| |_\\   |_ _| |_ _| |  \\ \\_\\  \\--'  /_| |__/ |_/ /    \\ \\_    |\\____) |_/ /    \\ \\_    /\\ ' /    _| |_\\ \\.___.'\\ ",
+  "  |_____|\\____|_____|____||____|\\.____.'|________|____|  |____|   |_______.'____|  |____|    \\_/    |_____|\\._____.' ",
+  "",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
